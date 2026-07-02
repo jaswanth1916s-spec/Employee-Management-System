07369d74ca5b71fbd9c9e3534197559d9ee92dc2
Jaswanth <jaswanth1916s@gmail.com>
2026-07-02 17:01:44 +0530

    Change DEBUG setting to default to True

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
